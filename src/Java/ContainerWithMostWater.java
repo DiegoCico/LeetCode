7b3dfa45098f3a630bf19f3a8e8eb13e28163ea8
Diego Cicotoste <cicotosted@gmail.com>
2024-04-01 12:14:32 -0400

    april 1 1.01

---
+++ src/Java/ContainerWithMostWater.java
@@ -0,0 +1,22 @@
+package Java;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0, right = height.length - 1;
+
+        while (left < right) {
+            int width = right - left;
+            int minHeight = Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, width * minHeight);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
