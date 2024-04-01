059c1b027e9120fa5774fa98a77d385a82243e66
Diego Cicotoste <cicotosted@gmail.com>
2024-04-01 11:02:40 -0400

    april 1

---
+++ src/Java/IntToRoman.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class IntToRoman {
+    public String intToRoman(int num) {
+        StringBuilder s = new StringBuilder();
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                s.append(symbols[i]);
+            }
+        }
+        return s.toString();
+    }
+}
