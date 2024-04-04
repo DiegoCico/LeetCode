0c7407a2f81f8ba427591e7edac02ea48d214aad
Diego Cicotoste <cicotosted@gmail.com>
2024-04-04 12:59:01 -0400

    april 4

---
+++ src/Java/MaxNestedDepth.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class MaxNestedDepth {
+        public int maxDepth(String s) {
+            int count = 0;
+            int maxNum = 0;
+            for (char c : s.toCharArray()) {
+                if (c == '(') {
+                    count++;
+                    if (maxNum < count)
+                        maxNum = count;
+                } else if (c == ')') {
+                    count--;
+                }
+            }
+            return maxNum;
+        }
+}
