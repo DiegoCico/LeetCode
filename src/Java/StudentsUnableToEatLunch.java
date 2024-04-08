0844d7bf3ec6e449894c10821b1fa3ce64166853
Diego Cicotoste <cicotosted@gmail.com>
2024-04-08 20:04:45 +0100

    april 8

---
+++ src/Java/StudentsUnableToEatLunch.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class StudentsUnableToEatLunch {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int[] count = new int[2];
+
+        for (final int student : students)
+            ++count[student];
+
+        for (int i = 0; i < sandwiches.length; ++i) {
+            if (count[sandwiches[i]] == 0)
+                return sandwiches.length - i;
+            --count[sandwiches[i]];
+        }
+
+        return 0;
+    }
+}
