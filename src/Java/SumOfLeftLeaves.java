8131207601d24abc6ee29154bbcb7da23c7875da
Diego Cicotoste <cicotosted@gmail.com>
2024-04-14 19:19:01 +0100

    april 14

---
+++ src/Java/SumOfLeftLeaves.java
@@ -0,0 +1,35 @@
+package Java;
+
+import javax.swing.tree.TreeNode;
+import java.util.LinkedList;
+
+public class SumOfLeftLeaves {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<Pair<TreeNode, Boolean>> queue = new LinkedList<>();
+        queue.offer(new Pair<>(root, false));
+        int totalSum = 0;
+
+        while (!queue.isEmpty()) {
+            Pair<TreeNode, Boolean> pair = queue.poll();
+            TreeNode node = pair.getKey();
+            boolean isLeft = pair.getValue();
+
+            if (isLeft && node.left == null && node.right == null) {
+                totalSum += node.val;
+            }
+
+            if (node.left != null) {
+                queue.offer(new Pair<>(node.left, true));
+            }
+            if (node.right != null) {
+                queue.offer(new Pair<>(node.right, false));
+            }
+        }
+
+        return totalSum;
+    }
+}
