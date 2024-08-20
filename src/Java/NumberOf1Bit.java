99a3b713644038f4e39fc2acbb5be9023264737a
Diego Cicotoste <cicotosted@gmail.com>
2024-08-20 13:40:08 -0400

    aug 20

---
+++ src/Java/NumberOf1Bit.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingWeight(int n) {
+        String binary = Integer.toBinaryString(n);
+        int counter = 0;
+        for(char c : binary.toCharArray())
+            if(c == '1')
+                counter++;
+        
+        return counter;
+    }
+}
